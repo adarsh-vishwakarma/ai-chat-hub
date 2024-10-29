4d5a76eb2dfafc83d37199bf885b660c8153203f
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2024-10-29 07:56:22 +0530

    Rendering PDF file

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,11 +1,72 @@
-const PdfRenderer = () => {
-    return <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
-        <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-        <div className='flex items-center gap-1.5'>
-            top bar
-        </div>
-        </div>
-    </div>
+"use client";
+import { ChevronDown, ChevronUp, Ghost, Loader2 } from "lucide-react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import { useToast } from "./ui/use-toast";
+import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useState } from "react";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+// pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.7.76/build/pdf.worker.min.js';
+
+interface PdfRenderProps {
+  url: string;
 }
 
-export default PdfRenderer
+const PdfRenderer = ({ url }: PdfRenderProps) => {
+  const { toast } = useToast();
+
+  const { width, ref } = useResizeDetector();
+  const [numPages, setNumPages] = useState<number>();
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">
+          <Button aria-label="previous-page" variant="ghost">
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5">
+            <Input className="w-12 h-8" />
+            <p className="text-zinc-700 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+          <Button aria-label="next-page" variant="ghost">
+            <ChevronUp className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex-1 w-full max-h-screen">
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading PDF",
+                description: "Please try again later",
+                variant: "destructive",
+              });
+            }}
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+            file={url}
+            className="max-h-full"
+          >
+            <Page width={width ? width : 1} pageNumber={1} />
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PdfRenderer;
